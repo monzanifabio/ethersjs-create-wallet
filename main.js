d63cb2700e2966c620b0c262be2634a45b23610f
FabioMonzani <renderbros@gmail.com>
2023-02-07 10:19:28 +0000

    Added tooltip and copy to clipboard function

--- main.js
+++ main.js
@@ -22,3 +22,39 @@ new QRCode(document.getElementById("qrcode"), {
   colorLight: "#141E26",
   correctLevel: QRCode.CorrectLevel.M,
 });
+
+let btnCopyAddress = document.getElementById("btnCopyAddress");
+let btnCopyMnemonic = document.getElementById("btnCopyMnemonic");
+let btnCopyPk = document.getElementById("btnCopyPk");
+
+btnCopyAddress.addEventListener("click", function () {
+  copytoClipboard(wallet.address);
+});
+btnCopyMnemonic.addEventListener("click", function () {
+  copytoClipboard(wallet.mnemonic.phrase);
+});
+btnCopyPk.addEventListener("click", function () {
+  copytoClipboard(wallet.privateKey);
+});
+
+function copytoClipboard(what) {
+  navigator.clipboard.writeText(what);
+  document.getElementById("tooltipBody").innerText = "Copied to clipboard!";
+  tooltip.style.display = "block";
+
+  setTimeout(function () {
+    tooltip.style.display = "none";
+  }, 2000);
+}
+
+const walletTextFile = wallet.address + "\n" + wallet.mnemonic.phrase + "\n" + wallet.privateKey;
+
+function downloadTextFile(text, fileName) {
+  const blob = new Blob([text], { type: "text/plain" });
+  const link = document.createElement("a");
+  link.download = fileName;
+  link.href = URL.createObjectURL(blob);
+  link.click();
+}
+
+// downloadTextFile(walletTextFile, "wallet.txt");
